703af05ad2b5bbbcb069a4a476b3dab9cef9679c
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-04 13:29:38 -0600

    create dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Dashboard extends Component {
     render() {
         return(
-            <div>This is the landing Dashboard</div>
+            <div>This is the Dashboard</div>
         )
     }
 }
